55a2cf4571bc550943e27824fb125cdc11d1b2bc
Alexander Khamitski <alexander.khamitski@winfinity.live>
2024-05-21 00:40:31 +0300

    [JSP and JSTL] task 1 was implemented

---
+++ src/main/java/com/TeachMeSkills/jsp/hw/servlets/RegisteredServlet.java
@@ -0,0 +1,36 @@
+package com.TeachMeSkills.jsp.hw.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class RegisteredServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String amount = request.getParameter("amount");
+        if (!isAnyValueNullOrEmpty(firstName, lastName, amount)) {
+            request.setAttribute("firstName", firstName);
+            request.setAttribute("lastName", lastName);
+            request.setAttribute("amount", amount);
+            getServletContext().getRequestDispatcher("/WEB-INF/registered.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/save-request.jsp").forward(request, response);
+        }
+    }
+
+    private boolean isAnyValueNullOrEmpty(String firstName, String lastName, String amount) {
+        return firstName.isEmpty()
+                || lastName.isEmpty()
+                || amount.isEmpty()
+                || firstName == null
+                || lastName == null
+                || amount == null;
+    }
+}
