195bbc22a06317a2be3b709594a7e3f11378a9fd
Alexander Khamitski <alexander.khamitski@winfinity.live>
2024-07-02 19:02:05 +0300

    [Spring, MVC, Controller, Interceptors] draft

---
+++ src/main/java/com/tms/lesson41/hw/DTO/User.java
@@ -0,0 +1,12 @@
+package com.tms.lesson41.hw.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+    private int id;
+    private String name;
+    private String login;
+}
---
+++ src/main/resources/db.migration/v-2-0-0/sql/create_user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "user"
+(
+    id   SERIAL,
+    name VARCHAR(20)
+)
